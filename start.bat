5d4d503640b6b587e2dc61f56f9bbcbb47cdd6e7
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-09-26 18:22:53 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -2,11 +2,11 @@
 
 cd C:\Users\celin\Documents\JULES\DANG\Compiler
 
-call C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /win32icon:icon.ico "C:\Users\celin\Documents\JULES\DANG\Compiler\program.cs" "C:\Users\celin\Documents\JULES\DANG\Compiler\executor.cs"
+call C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /win32icon:icon.ico dang.cs executor.cs
 
 echo compiled!
 pause
 cls
-call "C:\Users\celin\Documents\JULES\DANG\Compiler\program.exe" C:\Users\celin\Documents\JULES\DANG\Compiler\test.dang
+call dang.exe test.dang
 
 pause
